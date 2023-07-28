dcebd15ce5ea5d2afcb6a6e47d3ebaaa7c0d75d0
JuditRoca <roca.tovar@gmail.com>
2023-07-28 14:30:16 +0200

    act.textos+index.

---
+++ index.py
@@ -0,0 +1,34 @@
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+import sys
+import os
+import gradio as gr
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+os.chdir(os.path.dirname(__file__))
+Api_key = os.environ.get("OPENAI_API_KEY")
+
+def construct_index(directory_path, title):
+
+    # set number of output tokens
+    num_outputs = 2000
+   
+    # define prompt helper
+    prompt_helper = PromptHelper(4096, 2000, 20, 600)
+
+    # define LLM
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs, openai_api_key= Api_key))
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index.save_to_disk(title)
+
+    return index
+
+index = construct_index("Data_bot", "index.json")
+index = construct_index("Data_client", "index_client.json")
