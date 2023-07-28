4dc14a6b09ddc74b9cb2aa7649564592681db97c
JuditRoca <roca.tovar@gmail.com>
2023-07-28 14:31:02 +0200

    act.bot

--- app_lang.py
+++ app_lang.py
@@ -12,50 +12,29 @@ load_dotenv()
 os.chdir(os.path.dirname(__file__))
 Api_key = os.environ.get("OPENAI_API_KEY")
 
-def construct_index(directory_path):
+def chat_bot_gen(input_text):
+    index = GPTSimpleVectorIndex.load_from_disk("index.json")
+    response = index.query(input_text, response_mode="compact")
+    return response.response
 
-    
-    # set maximum input size
-    max_input_size = 4096
-    # set number of output tokens
-    num_outputs = 2000
-    # set maximum chunk overlap
-    max_chunk_overlap = 20
-    # set chunk size limit
-    chunk_size_limit = 600 
-
-    # define prompt helper
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-    # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs, openai_api_key= Api_key))
-    documents = SimpleDirectoryReader(directory_path).load_data()
-    
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    index.save_to_disk('index.json')
-
-    return index
-
-
-def chat_bot(input_text):
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+def chat_bot_client(input_text):
+    index = GPTSimpleVectorIndex.load_from_disk("index_client.json")
     response = index.query(input_text, response_mode="compact")
     return response.response
 
 app = Flask(__name__)
-index = construct_index("Data_bot")
+
 
 @app.route('/', methods=['GET'])
 def welcome():
     return '¡Hola! Bienvenido a Sol7. En que te puedo ayudar?.'
 
 @app.route('/chat', methods=['POST'])
-def chatbot_api():
+def chatbot_gen():
     try:
         question = request.args.get('question')
         if question:
-            response = chat_bot(question)
+            response = chat_bot_gen(question)
 
             # Create a response object with plain text content
             response_text = make_response(response)
@@ -63,10 +42,26 @@ def chatbot_api():
             
             return response_text
         else:
-            return "Lo siento, la pregunta debe ser proporcionada en el formulario."
+            return "Lo siento, no he entendido la pregunta."
     except Exception as e:
         return f"Error: {str(e)}"
     
+@app.route('/chat_client', methods=['POST'])
+def chatbot_client():
+    try:
+        question = request.args.get('question')
+        if question:
+            response = chat_bot_client(question)
+
+            # Create a response object with plain text content
+            response_text = make_response(response)
+            response_text.headers['Content-Type'] = 'text/plain'
+            
+            return response_text
+        else:
+            return "Lo siento, no he entendido la pregunta."
+    except Exception as e:
+        return f"Error: {str(e)}"    
     
 if __name__ == '__main__':
     app.run(debug=True)
